ca5041e485be41b3eb31a52a3c90074d49000cb0
voidoperator <192821839+voidoperator@users.noreply.github.com>
2026-07-15 20:00:24 +0200

    Refactor config constants (moved into `_BL_CONST`), add debugging messages and add config resolver (to know the value of a setting in an exact moment).

--- usr/share/bash-local/lib/shared/config.bash
+++ usr/share/bash-local/lib/shared/config.bash
@@ -6,30 +6,6 @@
 # @description Defines settings and implements functions responsible for
 # managing bl configuration.
 
-# MARK: Settings
-# -----------------------------------------------------------------------------
-# @section Settings definition
-#
-# ---
-#
-# | Setting | Type | Restriction |
-# |------|--------|-------|
-# | color | enum (auto, always, never) | - |
-# | debug | bool | - |
-# | verbose | bool | - |
-declare -grA _BL_CONFIG_TYPE=(
-
-    [${_BL_CONST[CONFIG_KEY_COLOR]}]=enum
-    [${_BL_CONST[CONFIG_KEY_DEBUG]}]=bool
-    [${_BL_CONST[CONFIG_KEY_VERBOSE]}]=bool
-)
-
-declare -grA _BL_CONFIG_ENUM=(
-
-    [${_BL_CONST[CONFIG_KEY_COLOR]}]="auto always never"
-)
-
-
 # MARK: Public
 # -----------------------------------------------------------------------------
 # @section Public funtions
@@ -39,32 +15,24 @@ declare -grA _BL_CONFIG_ENUM=(
 
 # @description Clears and loads all settings.
 #
-# Side effects:
-# - Reads "${_BL_CONST[CONFIG_FILE]}".
-# - Writes `_BL_STATE`
-#
 # @noargs
 #
 # @exitcode 0 Success.
 # @exitcode 1 Internal error.
-_bl_config_refresh() {
+bl_config_refresh() {
     
-    _bl_config_clear || return 1
-    _bl_config_load || return 1
+    bl_config_clear || return 1
+    bl_config_load || return 1
 }
 
-# @description Loads bl configuration according to config file
-# (`$_BL_CONST[CONFIG_FILE]`) and default settings in `_BL_CONST`.
-#
-# Side effects:
-# - Reads "${_BL_CONST[CONFIG_FILE]}".
-# - Writes `_BL_STATE`
+# @description Loads bl configuration according to config file and default
+# settings in `_BL_CONST`.
 #
 # @noargs
 #
 # @exitcode 0 Success.
 # @exitcode 1 Internal error.
-_bl_config_load() {
+bl_config_load() {
 
     local -a keys values
 
@@ -82,10 +50,10 @@ _bl_config_load() {
 #
 # @exitcode 0 Success.
 # @exitcode 1 `_BL_STATE` is not defined.
-_bl_config_clear() {
+bl_config_clear() {
 
     if ! declare -p _BL_STATE &>/dev/null; then
-        _bl_log_debug "FATAL_UNDEFINED_VARIABLE" "_BL_STATE"
+        bl_log_debug "FATAL_UNDEFINED_VARIABLE" "_BL_STATE"
         return 1
     fi
 
@@ -94,6 +62,37 @@ _bl_config_clear() {
         [[ $key == CONFIG_* ]] || continue
         _BL_STATE["$key"]=""
     done
+}
+
+# @description Decides value of setting according to order of priorities:
+# - Option value given by user and stored in `_BL_STATE[OPTION_<KEY>]`
+#   (optional).
+# - Environment variable defined as `BL_<KEY>=<value>` (optional).
+# - Configuration value in `_BL_STATE[CONFIG_<KEY>]` applied in starting
+#   procedure.
+#
+# Side effects:
+# - Reads `_BL_STATE`.
+#
+# @arg $1 string Key (lowercase).
+# @arg $2 string Reference to decided value.
+#
+# @exitcode 0 Success.
+# @exitcode 1 Invalid key or value.
+bl_config_resolve() {
+
+    local -r KEY="$1"    
+    local -n value_=$2
+    
+    local -r MAYUS_KEY="${KEY^^}"
+
+    if [[ -n "${_BL_STATE[OPTION_$MAYUS_KEY]:-}" ]]; then
+        value_="${_BL_STATE[OPTION_$MAYUS_KEY]}"
+    else
+        local -r KEY_VAR="BL_$MAYUS_KEY"
+        value_="${!KEY_VAR:-${_BL_STATE[CONFIG_$MAYUS_KEY]}}"
+    fi
+    _bl_config_validate "$KEY" "$value_" || return 1
 }
 
 
@@ -118,13 +117,10 @@ _bl_config_clear() {
 # ```
 #
 # Side effects:
-# - Reads `${_BL_CONST[CONFIG_FILE]}`.
-# - Logs FATAL_NO_CONFIG if config file does not exist.
-# - Logs FATAL_NO_PERM_CONFIG if cannot read config file.
-# - Logs ERROR_BAD_SETTING if setting format is invalid.
+# - Reads `_BL_CONST`.
 #
-# @arg $1 array  Reference to keys array.
-# @arg $2 array  Reference to values array.
+# @arg $1 array Reference to keys array.
+# @arg $2 array Reference to values array.
 #
 # @exitcode 0 Parsing succeeded, but could have found invalid settings.
 # @exitcode 1 Config file does not exist or does not have reading permissions.
@@ -133,8 +129,16 @@ _bl_config_parse() {
     local -n keys_=$1
     local -n values_=$2
 
-    [[ -f "${_BL_CONST[CONFIG_FILE]}" ]] || { _bl_log "FATAL_NO_CONFIG_FILE"; return 1; }
-    [[ -r "${_BL_CONST[CONFIG_FILE]}" ]] || { _bl_log "FATAL_NO_PERM_CONFIG_FILE"; return 1; }
+    if ! [[ -n "${_BL_CONST[PATH_CONFIG]:-}" ]]; then
+        bl_log_debug "FATAL_MISSING_VARIABLE" "_BL_CONST[PATH_CONFIG]"
+        return 1
+    elif ! [[ -f "${_BL_CONST[PATH_CONFIG]}" ]]; then
+        bl_log "FATAL_NO_FILE" "${_BL_CONST[PATH_CONFIG]}"
+        return 1
+    elif ! [[ -r "${_BL_CONST[PATH_CONFIG]}" ]]; then
+        bl_log "FATAL_NO_PERM" "${_BL_CONST[PATH_CONFIG]}"
+        return 1
+    fi
 
     local lineno=0
     local retval=0
@@ -147,15 +151,16 @@ _bl_config_parse() {
         [[ $line =~ ^[[:space:]]*$ ]] && continue
         [[ $line =~ ^[[:space:]]*[#] ]] && continue
 
-        if ! [[ $line =~ ${_BL_CONST[CONFIG_REGEX]} ]]; then
-            _bl_log_debug "ERROR_CONFIG_FILE_BAD_FORMAT" "$lineno"
+        if ! [[ $line =~ ${_BL_CONST[CONFIG_PARSE_REGEX]} ]]; then
+            
+            bl_log_debug "ERROR_CONFIG_PARSE_BAD_FORMAT" "$lineno"
             continue
         fi
 
         keys_+=("${BASH_REMATCH[1]}")
         values_+=("${BASH_REMATCH[2]}")
       
-    done < "${_BL_CONST[CONFIG_FILE]}"
+    done < "${_BL_CONST[PATH_CONFIG]}"
 }
 
 
@@ -169,9 +174,6 @@ _bl_config_parse() {
 #
 # If all pairs valid, function is a no-op.
 #
-# Side effects:
-# - Logs FATAL_UNEVEN_ARRAYS if array have different size.
-#
 # @arg $1 array Reference to keys array.
 # @arg $2 array Reference to values array.
 #
@@ -183,7 +185,7 @@ _bl_config_filter() {
     local -n values_=$2
 
     if [[ "${#keys_[@]}" -ne "${#values_[@]}" ]]; then
-        _bl_log_debug "FATAL_UNEVEN_ARRAYS" "${FUNCNAME[0]}"
+        bl_log_debug "FATAL_UNEVEN_ARRAYS"
         return 1
     fi
     [[ "${#keys_[@]}" -ne 0 ]] || return 0
@@ -216,35 +218,36 @@ _bl_config_filter() {
 # key-value pair is only validated by type validator.
 #
 # Side effects:
-# - Logs ERROR_CONFIG_FILE_BAD_KEY if key does not exist.
-# - Logs ERROR_CONFIG_FILE_BAD_VALUE if invalid value.
+# - Reads `_BL_CONST`.
 #
-# @arg $1 string Key.
+# @arg $1 string Key (lowercase).
 # @arg $2 string Value.
 #
 # @exitcode 0 Validation succeeds.
 # @exitcode 1 Given key is not defined in `_BL_CONFIG_TYPE`.
 # @exitcode 2 Validation fails.
 _bl_config_validate() {
-
+    
     local -r KEY="$1"
     local -r VALUE="$2"
 
-    if ! [[ -v _BL_CONFIG_TYPE[$KEY] ]]; then
-        _bl_log_debug "ERROR_CONFIG_FILE_BAD_KEY" "$KEY"
+    local -r MAYUS_KEY="${KEY^^}"
+
+    if ! [[ -v _BL_CONST[CONFIG_TYPE_"$MAYUS_KEY"] ]]; then
+        bl_log_debug "ERROR_CONFIG_VALIDATE_BAD_KEY" "$KEY"
         return 1
     fi
-    local -r TYPE=${_BL_CONFIG_TYPE[$KEY]}
-
+    local -r TYPE="${_BL_CONST[CONFIG_TYPE_"$MAYUS_KEY"]}"
+    
     if ! "_bl_config_validate_$TYPE" "$KEY" "$VALUE"; then
-        _bl_log_debug "ERROR_CONFIG_FILE_BAD_VALUE" "$VALUE" "$KEY"
+        bl_log_debug "ERROR_CONFIG_VALIDATE_BAD_VALUE" "$VALUE" "$KEY"
         return 2
     fi
-
+    
     # Extra restrictions
     if declare -F "_bl_config_validate_$KEY" >/dev/null; then
         if ! "_bl_config_validate_$KEY" "$VALUE"; then
-            _bl_log_debug "ERROR_CONFIG_FILE_BAD_VALUE" "$VALUE" "$KEY"
+            bl_log_debug "ERROR_CONFIG_VALIDATE_BAD_VALUE" "$VALUE" "$KEY"
             return 2
         fi
     fi
@@ -257,8 +260,6 @@ _bl_config_validate() {
 #
 # @exitcode 0 Validation succeeds.
 # @exitcode 1 Validation fails.
-#
-# @see Used in [_bl_config_validate](#_bl_config_validate)
 _bl_config_validate_bool() {
 
     local -r VALUE="$2"
@@ -267,22 +268,20 @@ _bl_config_validate_bool() {
 
 # @description Validates an enum according to valid values in `_BL_CONFIG_ENUM`.
 #
-# @arg $1 string Key.
+# @arg $1 string Key (lowercase).
 # @arg $2 string Value.
 #
 # @exitcode 0 Validation succeeds.
 # @exitcode 1 Validation fails.
-#
-# @see Used in [_bl_config_validate](#_bl_config_validate)
 _bl_config_validate_enum() {
 
-    local -r KEY="$1"
+    local -r MAYUS_KEY="${1^^}"
     local -r VALUE="$2"
-
+    
     local option
-
-    for option in ${_BL_CONFIG_ENUM[$KEY]}; do
-        [[ $VALUE == "$option" ]] && return 0
+    
+    for option in ${_BL_CONST[CONFIG_ENUM_"$MAYUS_KEY"]}; do
+        [[ $option == "$VALUE" ]] && return 0
     done
     
     return 1
@@ -315,20 +314,12 @@ _bl_config_apply() {
     local -rn VALUES=$2
 
     if ! declare -p _BL_STATE &>/dev/null; then
-        _bl_log_debug "FATAL_UNDEFINED_VARIABLE" "_BL_STATE"
+        bl_log_debug "FATAL_UNDEFINED_VARIABLE" "_BL_STATE"
         return 1
     fi
 
     local i
     for (( i = 0; i < ${#KEYS[@]}; i++)); do
         _BL_STATE[CONFIG_"${KEYS[i]^^}"]="${VALUES[i]}"
-    done
-
-    local default_key
-    for default_key in "${!_BL_CONST[@]}"; do
-
-        [[ $default_key == DEFAULT_CONFIG_* ]] || continue
-        key="${default_key#DEFAULT_}"
-        : "${_BL_STATE[$key]:=${_BL_CONST[$default_key]}}"
     done
 }
